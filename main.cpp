7d90ee5374b7fc3368f60f002f6f3b092a370869
Lukas-k03 <98773707+Lukas-k03@users.noreply.github.com>
2022-09-11 23:39:41 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <cstdlib>
+
 //160
 void addBook ();
 void addMem ();
@@ -46,32 +49,44 @@ int main (){
 }
 
 void addBook() {
+    
     int index,i;
-    while(true){
+    char c;
     std :: string bookName,authorFirstName,authorLastName,isbn;
+  
+    while(true){
+        
+        std :: cout << "Enter Name Of Book:";
+       
+        std :: cin.sync(); //Discards unread characters, makes get line work
+        std :: getline(std :: cin, bookName);  
+       
+        std :: cout << "Enter First Name Of Author:";
+        std :: cin >> authorFirstName;
+        
+        std :: cout << "Enter Last Name Of Author:";
+        std :: cin >> authorLastName;
+       
+        std :: cout << "Enter ISBN:";
+        std :: cin >> isbn;
     
-    std :: cout << "Enter Name Of Book:";
-    std :: cin >> bookName; 
-    std :: cout << "Enter First Name Of Author:";
-    std :: cin >> authorFirstName;
-    std :: cout << "Enter Last Name Of Author:";
-    std :: cin >> authorLastName;
-    std :: cout << "Enter ISBN:";
-    std :: cin >> isbn;
+        std :: string allData = bookName + "|" + authorFirstName + "|" + authorLastName + "|" + isbn + "\n";
+        //opens the file, then writes the string to it.
+        std :: ofstream bookListData; //object
+            bookListData.open("bookList.txt");
+            int i=0;
+            for(;i!=(bookListData.eof());i++){
+                bookListData.seekp(bookListData.find());
+            }
+        bookListData << allData;
+        bookListData.close();
+        // Work On this Part
+        index++;
     
-    std :: string write = bookName + "|" + authorFirstName + "|" + authorLastName + "|" + isbn + "\n";
-    
-    //opens the file, then writes the string to it.
-    std :: ofstream bookListData;
-    bookListData.open("bookList.txt");
-    bookListData << write;
-    bookListData.close();
-    // 
-    index++;
-    
-    std :: cout << "Press Esc to Quit\nPress Enter To Add Another Book";
-    if(getchar()==27){break;};
-    }
-    //problem is here check it
+        std :: cout << "Press ` to Quit\nPress Enter To Add Another Book";
+        std :: cin >> c;
+        if(c=='`') {break;};
+        
+     }
     
     }
